7796ccd5eeb2525c8317b179dbb3286c4c045a24
Arm37e3 <akbbna48@gmail.com>
2024-01-31 12:21:35 +0700

    update 31-1-2024 time 12.19pm

--- controller/incomeContainer.js
+++ controller/incomeContainer.js
@@ -1,14 +1,15 @@
 const db = require('../dbconnect/dbconn');
 
-exports.index = (req,res,next)=>{
 
+exports.index = async (req,res,next)=>{
     db.query(
         'SELECT * FROM income',(err, result)=>{
                 res.status(200).json(result);
         }
     )
 }
-exports.delete = (req,res,next)=>{
+
+exports.delete = async(req,res,next)=>{
     try {
         const {id} = req.params;
         console.log(id);
@@ -26,4 +27,24 @@ exports.delete = (req,res,next)=>{
             }
         })
     }
+}
+exports.insert= async(req, res, next) => {
+    const {text , amount} = req.body 
+    console.log(text);
+    console.log(amount);
+    db.query( `INSERT INTO income (text,amount) VALUES(?,?)` ,
+        [text,amount] ,
+        (err, result) => {
+            if(err){
+                console.error('Error inserting data',err);
+                res.status(500).json({
+                    message: 'Error inserting data'
+                })
+            }else{
+                res.status(200).json({
+                    message: 'Income inserted successfully'
+                })
+            }
+        })
+   
 }
--- routes/income.js
+++ routes/income.js
@@ -6,4 +6,6 @@ const incomeContainer = require('../controller/incomeContainer')
 
 router.get('/', incomeContainer.index); 
 router.delete('/:id', incomeContainer.delete);
+router.post('/', incomeContainer.insert);
+
 module.exports = router;
